de664ae5603e0c723a838b4844d73842ce75500e
Ken20215 <k.ishioka@renk.co.jp>
2024-10-08 11:07:00 +0900

    CieclCIの練習アプリを作成

---
+++ CirclCISample/CirclCISample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CirclCISample
+//
+//  Created by *石岡顕* on 2024/10/08.
+//  Copyright (c) 2024 *ReNKCHANNEL*. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
